b43fe8eb0f0a5f3322e70b57108ee19d58b186ab
Swayam792 <prakashswayam792@gmail.com>
2022-05-31 22:44:14 +0530

    Day-1 Completed

--- Array/Kth_smallest_element_in_the_array.cpp
+++ Array/Kth_smallest_element_in_the_array.cpp
@@ -1,15 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-
- // } Driver Code Ends
-//User function template for C++
-
+ 
 class Solution{
     public:
-    // arr : given array
-    // l : starting index of the array i.e 0
-    // r : ending index of the array i.e size-1
-    // k : find kth smallest element and return using this function
+    
     int kthSmallest(int arr[], int l, int r, int k) {
         //code here
          priority_queue<int> q;
