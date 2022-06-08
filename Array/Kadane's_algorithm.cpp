e8533a8de347437135ab9ab7a9c963d89e7d0a85
Swayam792 <prakashswayam792@gmail.com>
2022-06-09 01:07:33 +0530

    Day-5 Completed

---
+++ Array/Kadane's_algorithm.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+class Solution{
+    public:
+    
+    long long maxSubarraySum(int arr[], int n){
+         
+        long long sum = 0,maxi = INT_MIN;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            maxi = max(maxi, sum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+};
+ 
+int main()
+{
+    int t,n;
+    
+    cin>>t;  
+    while(t--)  
+    {
+        
+        cin>>n;  
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i];  
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+} 
+
+// Time Complexity - O(N) Auxiliary Space - O(1)
